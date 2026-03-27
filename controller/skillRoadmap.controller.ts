e656043b7f489cfaa316a514692d67d4d204ac75
ShashankPhiskeKanaka <shashank.phiske@kanakasoftware.com>
2026-03-27 15:16:38 +0530

    Skill roadmap system

---
+++ controller/skillRoadmap.controller.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import { ControllerMessages } from "../constants/controller.messages.js";
+import type { SkillService } from "../service/skill.service.js";
+import { logger } from "../utils/logger.js";
+import { BaseController } from "./base.controller.js";
+import { ApiResponse } from "../utils/api.utils.js";
+import type { SkillRoadmapService } from "../service/skillRoadmap.service.js";
+
+const controllerMessages = new ControllerMessages("Skill roadmap");
+
+class SkillRoadmapController extends BaseController<SkillRoadmapService> {
+    constructor(service: SkillRoadmapService) {
+        super(service, "Skill roadmap");
+    }
+
+    fetchAll = async (req: Request, res: Response) => {
+        logger.http(controllerMessages.FETCHALL.req, {
+            ip: req.ip,
+            userId: req.user?.id
+        });
+
+        const skillRoadmaps = await this.service.fetchAll(
+            this.getPagination(req),
+            {
+                category: req.query.category?.toString()
+            },
+            [
+                "name",
+                "category"
+            ]
+        )
+
+        return ApiResponse.success(res, controllerMessages.FETCHALL.res, skillRoadmaps);
+    }
+
+}
+
+export { SkillRoadmapController }
---
+++ router/skillRoadmap.router.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { ControllerFactory } from "../factory/general.factory.js";
+import { SkillRoadmapRepository } from "../repository/skillRoadmap.repository.js";
+import { SkillRoadmapService } from "../service/skillRoadmap.service.js";
+import { SkillRoadmapController } from "../controller/skillRoadmap.controller.js";
+import { authenticate, authenticateAdmin } from "../middleware/authenticate.middleware.js";
+import { errorHandler } from "../factory/auth.factory.js";
+import { validate } from "../middleware/zod.middleware.js";
+import { DeleteData, GetData } from "../schema/general.schema.js";
+import { CreateSkillRoadmap, UpdateSkillRoadmap } from "../schema/skillRoadmap.schema.js";
+
+const router = express.Router();
+const controller = ControllerFactory.create(SkillRoadmapRepository, SkillRoadmapService, SkillRoadmapController);
+
+router.use(authenticate);
+router.get("/", errorHandler.controllerWrapper(controller.fetchAll));
+router.get("/:id", errorHandler.controllerWrapper(validate(GetData)), errorHandler.controllerWrapper(controller.fetch));
+
+router.use(authenticateAdmin);
+router.post("/", errorHandler.controllerWrapper(validate(CreateSkillRoadmap)), errorHandler.controllerWrapper(controller.create));
+router.patch("/:id", errorHandler.controllerWrapper(validate(UpdateSkillRoadmap)), errorHandler.controllerWrapper(controller.update));
+router.delete("/:id", errorHandler.controllerWrapper(validate(DeleteData)), errorHandler.controllerWrapper(controller.delete));
+
+export { router as SkillRoadmapRouter };
